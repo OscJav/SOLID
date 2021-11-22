715763094f495060f53d14f75ead8bec31890b27
Oscar J.Malaver <omalaver@ibero.edu.co>
2021-11-22 17:04:29 -0500

    firts commit

---
+++ Principio Sustitución de Liskov/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Para este ejercicio se crea la clase NOTICIERO  y luego se cre la clase Radio como hija__Oscar Malaver
+// Para luego sustituir a la clase NOTICIERO con la clase Radio y demostrar que se cumple el principio se sustitución de Liskov
+namespace Principio_Sustitución_de_Liskov
+{
+    internal class Noticiero
+    {
+        protected string mensaje;
+
+        public Noticiero(String pMensaje)
+        {
+            mensaje = pMensaje;
+        }
+        public void Muestra()
+        {
+            Console.WriteLine("Desde el noticiero: {0}",mensaje);
+        }
+    }
+}
---
+++ Principio Sustitución de Liskov/Class2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Para este ejercicio se crea la clase NOTICIERO  y luego se cre la clase Radio como hija __Oscar Malaver
+// Para luego sustituir a la clase NOTICIERO con la clase Radio y demostrar que se cumple el principio se sustitución de Liskov
+namespace Principio_Sustitución_de_Liskov
+{
+    internal class Radio : Noticiero
+    {
+        public Radio(String pMensaje) : base(pMensaje)
+        {
+
+        }
+        public new void Muestra()
+        {
+            Console.WriteLine("Desde el radio: {0}", mensaje);
+        }
+    }
+}
